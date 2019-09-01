4b9b52505259255a1c031d670fdb2ff6d8c77466
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-01 22:33:11 +0800

    add pql parser class

---
+++ Team00/Code00/src/spa/src/PQLParser.cpp
@@ -0,0 +1,13 @@
+#include "PQLParser.h"
+
+using namespace std;
+
+/*! 
+Parses and validates the query. If validation succeeds, returns the list of results.
+@param query The user's query statement.
+*/
+list<string> pql_parse_query(std::string query) {
+	list<string> results;
+
+	return results;
+}
---
+++ Team00/Code00/src/spa/src/PQLParser.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <list>
+
+std::list<std::string>& pql_parse_query(std::string query);
