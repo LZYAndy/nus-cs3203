ccbb4ef352864db01f8e7c0cdf241699c881952e
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-02 01:10:39 +0800

    update pql parser method

--- Team00/Code00/src/spa/src/PQLParser.h
+++ Team00/Code00/src/spa/src/PQLParser.h
@@ -2,6 +2,11 @@
 
 #include <string>
 #include <iostream>
-#include <list>
+#include <vector>
 
-std::list<std::string>& pql_parse_query(std::string query);
+#include <pql_dto\Entity.h>
+#include <pql_dto\Relationships.h>
+#include <pql_dto\Pattern.h>
+
+void pql_parse_query(std::string query, vector<pql_dto::Entity>& declaration_clause, 
+	vector<pql_dto::Entity>& select_clause, vector<pql_dto::Relationships>& such_that_clause, vector<pql_dto::Pattern>& pattern_clause);
