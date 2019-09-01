a78276c5d5b4c52ebf3352aae5fb2c07833e0f73
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-02 00:56:06 +0800

    remove build_win

---
+++ Team00/Code00/src/spa/src/pql_dto/ParentRelationship.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#include "Relationships.h"
+
+namespace pql_dto {
+	class ParentRelationship : public Relationships
+	{
+	public:
+		void ParentRelationship::set_relationship() {
+
+		}
+
+		void ParentRelationship::set_first_param() {
+
+		}
+
+		void ParentRelationship::set_second_param() {
+
+		}
+	};
+}
