1f0c41a8fdc593db21d3de3c9a9aa87abed3d7fd
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-03 01:03:11 +0800

    add PQLValidator class

---
+++ Team00/Code00/src/spa/src/PQLValidator.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+class PQLValidator
+{
+public:
+	static std::string pql_validate_initial_query(std::string query);
+	static std::string pql_validate_declaration_clause(std::string query);
+	static std::string pql_validate_select_clause(std::string query);
+	static std::string pql_validate_such_that_clause(std::string query);
+	static std::string pql_validate_pattern_clause(std::string query);
+};
