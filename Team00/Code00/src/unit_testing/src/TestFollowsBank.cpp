3e4990d634b61853da9a6040ed3abaab70bf1fd0
zenious <7553066+Zenious@users.noreply.github.com>
2019-09-09 17:25:07 +0800

    Init test files

--- Team00/Code00/src/unit_testing/src/TestFollowsBank.cpp
+++ Team00/Code00/src/unit_testing/src/TestFollowsBank.cpp
@@ -8,5 +8,7 @@ TEST_CASE("FollowBank<int, int> Test")
     follows_bank.put(1,2);
     follows_bank.put(2, 3);
     std::vector<int> result = follows_bank.get_all_keys();
+    std::sort(result.begin(), result.end(), std::greater<int>());
+    std::sort(expected_result.begin(), expected_result.end(), std::greater<int>());
     REQUIRE(result == expected_result);
 }
---
+++ Team00/Code00/src/unit_testing/src/TestParentBank.cpp
@@ -0,0 +1,7 @@
+#include "catch.hpp"
+#include "ParentBank.h"
+
+TEST_CASE("Init Parent Bank")
+{
+    ParentBank parent_bank;
+}
