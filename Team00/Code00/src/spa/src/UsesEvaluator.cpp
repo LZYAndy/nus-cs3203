b079fe97a889bbf50449b1a5d573f06781c966f6
刘照宇 <>
2019-09-10 15:46:12 +0800

    implement UsesEvaluator

--- Team00/Code00/src/spa/src/UsesEvaluator.cpp
+++ Team00/Code00/src/spa/src/UsesEvaluator.cpp
@@ -12,41 +12,41 @@ unordered_map<string, vector<string>> UsesEvaluator::evaluate_non_trivial(pql_dt
     {
         if (QueryUtility::is_integer(first_name))
         { // e.g. Uses(1, v)
-
+            result = QueryUtility::mapping(second_param, PKB::get_used_by_statement(stoi(first_name)));
         }
         else if (QueryUtility::is_proc_name(first_param))
         { // e.g. Uses("main", v)
-
+            result = QueryUtility::mapping(second_param, PKB::get_used_by_procedure(first_name));
         }
         else if (QueryUtility::is_proc_declared(first_param))
         { // e.g. Uses(p, v)
-
+            result = QueryUtility::mapping(first_param, second_param, PKB::get_all_uses_procedure_relationship());
         }
         else
         { // e.g. Uses(s, v)
-
+            result = QueryUtility::mapping(first_param, second_param, PKB::get_all_uses_statement_relationship());
         }
     }
     else if (QueryUtility::is_proc_declared(first_param))
     {
         if (second_name == "_")
         { // e.g. Uses(p, _)
-
+            result = QueryUtility::mapping(first_param, PKB::get_all_uses_procedure());
         }
         else if (QueryUtility::is_var_name(second_param))
         { // e.g. Uses(p, "x")
-
+            result = QueryUtility::mapping(first_param, PKB::get_procedures_uses(second_name));
         }
     }
     else
     {
         if (second_name == "_")
         { // e.g. Uses(s, _)
-
+            result = QueryUtility::mapping(first_param, PKB::get_all_uses_statement());
         }
         else if (QueryUtility::is_var_name(second_param))
         { // e.g. Uses(s, "x")
-
+            result = QueryUtility::mapping(first_param, PKB::get_statements_uses(second_name));
         }
     }
 
@@ -64,22 +64,22 @@ bool UsesEvaluator::evaluate_trivial(pql_dto::Entity first_param,
     {
         if (QueryUtility::is_var_name(second_param))
         { // e.g. Uses(1, "x")
-
+            return PKB::is_uses(stoi(first_name), second_name);
         }
         else if (second_name == "_")
         { // e.g. Uses(1, _)
-
+            return !PKB::get_used_by_statement(stoi(first_name)).empty();
         }
     }
     else if (QueryUtility::is_proc_name(first_param))
     {
         if (QueryUtility::is_var_name(second_param))
         { // e.g. Uses("main", "x")
-
+            return PKB::is_uses(first_name, second_name);
         }
         else if (second_name == "_")
         { // e.g. Uses("main", _)
-
+            return !PKB::get_used_by_procedure().empty(first_name);
         }
     }
 }
