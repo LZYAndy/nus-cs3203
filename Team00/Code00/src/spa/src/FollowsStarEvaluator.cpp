c4d417b5a819b895bb0a0244c331be035aac543c
刘照宇 <>
2019-09-10 12:38:39 +0800

    implement ParentEvaluator

--- Team00/Code00/src/spa/src/FollowsStarEvaluator.cpp
+++ Team00/Code00/src/spa/src/FollowsStarEvaluator.cpp
@@ -32,7 +32,6 @@ unordered_map<string, vector<string>> FollowsStarEvaluator::evaluate_non_trivial
 
 bool FollowsStarEvaluator::evaluate_trivial(pql_dto::Entity first_param,
         pql_dto::Entity second_param) {
-    string result;
     vector<string> empty_vec;
     string first_name = first_param.get_entity_name();
     string second_name = second_param.get_entity_name();
