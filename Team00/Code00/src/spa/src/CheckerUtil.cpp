4769765842c9d9dd50251c99ecfc666d694da90e
etjk <edricteojk@hotmail.com>
2019-09-11 17:36:07 +0800

    Add expression checker

--- Team00/Code00/src/spa/src/CheckerUtil.cpp
+++ Team00/Code00/src/spa/src/CheckerUtil.cpp
@@ -1,8 +1,10 @@
 #include "CheckerUtil.h"
 
+regex all_word("\\w+");
 regex valid_name("^[a-zA-Z][a-zA-Z0-9]*$");
 regex valid_const("^\\d+$");
-regex valid_expr("");
+//(_word_)  (_[+-*/%]_(_word_))*  <- Do not remove the space.
+regex valid_expr("^([\\(\\s]*(\\w*)[\\s\\)]*)(\\s*[+\\-*\\/%]\\s*([\\(\\s]*(\\w+)[\\s\\)]*))*$");
 
 bool CheckerUtil::is_name_valid(string stmt)
 {
@@ -22,9 +24,31 @@ bool CheckerUtil::is_const_valid(string stmt)
     return true;
 }
 
+// Note: Statement containing empty spaces only is considered valid for this function.
 bool CheckerUtil::is_expr_valid(string stmt)
 {
-    return false;
+    if (!is_bracket_balanced(stmt))
+    {
+        return false;
+    }
+
+    if (!regex_match(stmt, valid_expr))
+    {
+        return false;
+    }
+
+    smatch match;
+    while (regex_search(stmt, match, all_word)) // Get all word, checks performed in while.
+    {
+        string current_word = match[0];
+        stmt = match.suffix().str(); // Remove current word from statement.
+        if (!is_name_valid(current_word) && !is_const_valid(current_word)) // Invalid type.
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool CheckerUtil::is_condition_valid(string stmt)
