3077f1b3e1b9c209f8e88f62efb6a49e8d5a3b27
liuxuanian <liuxuan2472@gmail.com>
2019-09-13 17:06:59 +0800

    Procedure class added on.

---
+++ Team00/Code00/src/spa/src/Procedure.cpp
@@ -0,0 +1,9 @@
+#include "Procedure.h"
+
+string Procedure::get_name() {
+    return proc_name;
+}
+
+string Procedure::get_body() {
+    return body_of_proc;
+}
---
+++ Team00/Code00/src/spa/src/Procedure.h
@@ -0,0 +1,20 @@
+#ifndef AUTOTESTER_PROCEDURE_H
+#define AUTOTESTER_PROCEDURE_H
+
+#include <string>
+
+using namespace std;
+
+class Procedure{
+private:
+    string proc_name;
+    string body_of_proc;
+public:
+    // Constructor
+    Procedure(string, string);
+
+    string get_name();
+    string get_body();
+};
+
+#endif //AUTOTESTER_PROCEDURE_H
