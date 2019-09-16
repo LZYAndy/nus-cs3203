5d4f01f5358e030161efe796247567e074b4d029
Liu Xiaohang <>
2019-09-16 20:11:16 +0800

    Finish UsesBank test cases and fix bugs

--- Team00/Code00/src/spa/src/UsesBank.cpp
+++ Team00/Code00/src/spa/src/UsesBank.cpp
@@ -37,17 +37,17 @@ bool UsesBank::is_uses(std::string procedure, std::string variable)
     }
     else
     {
-        std::vector<std::string> tempBank = proc_bank.get(procedure);
-        std::vector<int> myVector;
-        for (std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); ++it)
+        std::vector<std::string> elements = proc_bank.get(procedure);
+        std::vector<std::string>::iterator ret;
+        ret = std::find(elements.begin(), elements.end(), variable);
+        if(ret == elements.end())
         {
-            int index = std::distance(myVector.begin(), it);
-            if (tempBank[index].compare(variable) == 0)
-            {
-                return true;
-            }
+            return false;
         }
-        return false;
+        else
+        {
+            return true;
+        }
     }
 }
 
@@ -59,17 +59,17 @@ bool UsesBank::is_uses(int statement, std::string variable)
     }
     else
     {
-        std::vector<std::string> tempBank = stmt_bank.get(statement);
-        std::vector<int> myVector;
-        for (std::vector<int>::iterator it = myVector.begin(); it != myVector.end(); ++it)
+        std::vector<std::string> elements = stmt_bank.get(statement);
+        std::vector<std::string>::iterator ret;
+        ret = std::find(elements.begin(), elements.end(), variable);
+        if(ret == elements.end())
         {
-            int index = std::distance(myVector.begin(), it);
-            if (tempBank[index].compare(variable) == 0)
-            {
-                return true;
-            }
+            return false;
         }
-        return false;
+        else
+        {
+            return true;
+        }
     }
 }
 
