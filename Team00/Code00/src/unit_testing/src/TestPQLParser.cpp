84f51f2684a0b398de0f56028c4f4ce06ac551df
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-16 15:25:21 +0800

    add test cases for pql query parser

--- Team00/Code00/src/unit_testing/src/TestPQLParser.cpp
+++ Team00/Code00/src/unit_testing/src/TestPQLParser.cpp
@@ -30,5 +30,17 @@ TEST_CASE("Pql query parser parse and validate correctly.")
         REQUIRE(such_that_clause.size() == 1);
         REQUIRE(pattern_clause.size() == 1);
     }
+
+    SECTION("Valid query 3.")
+    {
+        std::string test_query = "         variable v1\t, v2\n; assign a; Select v1 pattern a(_,          _) such  \v that    Modifies    (4, v1)";
+        std::string error = PQLParser::pql_parse_query(test_query, declaration_clause, select_clause, such_that_clause, pattern_clause);
+
+        REQUIRE(error == "");
+        REQUIRE(declaration_clause.size() == 3);
+        REQUIRE(select_clause.size() == 1);
+        REQUIRE(such_that_clause.size() == 1);
+        REQUIRE(pattern_clause.size() == 1);
+    }
 }
 
