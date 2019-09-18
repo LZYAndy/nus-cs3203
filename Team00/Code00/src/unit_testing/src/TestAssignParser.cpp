6f3eca4242ec97ca8805a3d71693cde1619ab418
etjk <edricteojk@hotmail.com>
2019-09-18 21:41:41 +0800

    Fix condition checker bug

--- Team00/Code00/src/unit_testing/src/TestAssignParser.cpp
+++ Team00/Code00/src/unit_testing/src/TestAssignParser.cpp
@@ -4,7 +4,4 @@
 
 TEST_CASE("AssignParser")
 {
-//PKB pkb;
-//Statement stmt = Statement(EntityType::ASSIGN, 1, "10");
-//REQUIRE_THROWS_WITH(AssignParser (pkb, stmt, "Parent"), "Invalid assignment statement");
 }
