3bc4bf5f020302c65dd537cc8bcb936a7ee5349c
etjk <edricteojk@hotmail.com>
2019-09-19 23:32:05 +0800

    Fix unit test build error

--- Team00/Code00/src/unit_testing/src/TestReadParser.cpp
+++ Team00/Code00/src/unit_testing/src/TestReadParser.cpp
@@ -3,12 +3,13 @@
 
 TEST_CASE("ReadParser")
 {
-REQUIRE_THROWS_WITH(ReadParser(PKB (), Statement(EntityType::READ, 1, "x"), "parent"), "Invalid read statement");
-REQUIRE_THROWS_WITH(ReadParser(PKB (), Statement(EntityType::READ, 1, "read "), "parent"), "Invalid read statement");
-REQUIRE_THROWS_WITH(ReadParser(PKB (), Statement(EntityType::READ, 1, "read a_"), "parent"), "Invalid read statement");
-REQUIRE_THROWS_WITH(ReadParser(PKB (), Statement(EntityType::READ, 1, "read a a"), "parent"), "Invalid read statement");
-REQUIRE_THROWS_WITH(ReadParser(PKB (), Statement(EntityType::ANY, 1, "read a"), "parent"), "Incorrect read type");
-REQUIRE_NOTHROW(PKB (), Statement(EntityType::READ, 1, "read x"), "parent");
-REQUIRE_NOTHROW(PKB (), Statement(EntityType::READ, 1, " read x1a "), "0");
-REQUIRE_NOTHROW(PKB (), Statement(EntityType::READ, 1, "       read            x            "), "0");
+    PKB pkb;
+    REQUIRE_THROWS_WITH(ReadParser(pkb, Statement(EntityType::READ, 1, "x"), "parent"), "Invalid read statement");
+    REQUIRE_THROWS_WITH(ReadParser(pkb, Statement(EntityType::READ, 1, "read "), "parent"), "Invalid read statement");
+    REQUIRE_THROWS_WITH(ReadParser(pkb, Statement(EntityType::READ, 1, "read a_"), "parent"), "Invalid read statement");
+    REQUIRE_THROWS_WITH(ReadParser(pkb, Statement(EntityType::READ, 1, "read a a"), "parent"), "Invalid read statement");
+    REQUIRE_THROWS_WITH(ReadParser(pkb, Statement(EntityType::ANY, 1, "read a"), "parent"), "Incorrect read type");
+    REQUIRE_NOTHROW(pkb, Statement(EntityType::READ, 1, "read x"), "parent");
+    REQUIRE_NOTHROW(pkb, Statement(EntityType::READ, 1, " read x1a "), "0");
+    REQUIRE_NOTHROW(pkb, Statement(EntityType::READ, 1, "       read            x            "), "0");
 }
