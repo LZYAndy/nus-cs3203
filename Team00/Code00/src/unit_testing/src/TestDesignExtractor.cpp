686ca6664b151ca1eddf806cbfb30dcd6436879a
zenious <7553066+Zenious@users.noreply.github.com>
2019-09-20 09:24:38 +0800

    fix bug on parent_star

--- Team00/Code00/src/unit_testing/src/TestDesignExtractor.cpp
+++ Team00/Code00/src/unit_testing/src/TestDesignExtractor.cpp
@@ -79,11 +79,17 @@ TEST_CASE("DesignExtractor::extract_parent_star()")
         REQUIRE(parent_star_bank.does_parent_star_exist());
         REQUIRE(parent_star_bank.get_all_parent_star().size() == 2);
         REQUIRE(parent_star_bank.get_all_children_star().size() == 2);
-        std::vector<int> result = parent_star_bank.get_parent_star(1);
-        std::vector<int> expected({2, 3});
-        std::sort(result.begin(), result.end());
-        std::sort(expected.begin(), expected.end());
-        REQUIRE(result == expected);
+        std::vector<int> result_parent = parent_star_bank.get_parent_star(3);
+        std::vector<int> expected_parent({2, 1});
+        std::sort(result_parent.begin(), result_parent.end());
+        std::sort(expected_parent.begin(), expected_parent.end());
+        REQUIRE(result_parent == expected_parent);
+
+        std::vector<int> result_child = parent_star_bank.get_children_star(1);
+        std::vector<int> expected_child({2, 3});
+        std::sort(result_child.begin(), result_child.end());
+        std::sort(expected_child.begin(), expected_child.end());
+        REQUIRE(result_child == expected_child);
     }
 
         SECTION("extract >1 stmt ParentBank 4-level depth")
@@ -100,10 +106,16 @@ TEST_CASE("DesignExtractor::extract_parent_star()")
         REQUIRE(parent_star_bank.does_parent_star_exist());
         REQUIRE(parent_star_bank.get_all_parent_star().size() == 4);
         REQUIRE(parent_star_bank.get_all_children_star().size() == 5);
-        std::vector<int> result = parent_star_bank.get_parent_star(1);
-        std::vector<int> expected({2, 3, 4, 5, 6});
+        std::vector<int> result = parent_star_bank.get_parent_star(6);
+        std::vector<int> expected({1, 5});
         std::sort(result.begin(), result.end());
         std::sort(expected.begin(), expected.end());
         REQUIRE(result == expected);
+
+        std::vector<int> result_child = parent_star_bank.get_children_star(1);
+        std::vector<int> expected_child({2, 3, 4, 5, 6});
+        std::sort(result_child.begin(), result_child.end());
+        std::sort(expected_child.begin(), expected_child.end());
+        REQUIRE(result_child == expected_child);
     }
 }
