f230659e32f1c2b95d15015c73f2a20f0428f67d
liuxuanian <liuxuan2472@gmail.com>
2019-09-22 00:17:30 +0800

    integration testing for if parser and while parser.

--- Team00/Code00/src/unit_testing/src/TestIfParser.cpp
+++ Team00/Code00/src/unit_testing/src/TestIfParser.cpp
@@ -16,6 +16,13 @@ TEST_CASE("Test if parser successfully.")
     Statement else_s = Statement(EntityType::PRINT, 3, "print y;");
     then_p.push_back(then_s);
     else_p.push_back(else_s);
+    if_statement.set_first_block(then_p);
+    if_statement.set_second_block(else_p);
     PKB pkb = PKB();
     IfParser ifParser = IfParser(pkb, if_statement, "main");
+    std::vector<int> parent = pkb.get_all_parent();
+    std::vector<int> children = pkb.get_all_children();
+
+    REQUIRE(parent.size() == 1);
+    REQUIRE(children.size() == 2);
 }
