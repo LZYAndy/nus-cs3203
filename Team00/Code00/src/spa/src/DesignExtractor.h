b4c85916a370a83c9b43903bcb131641489aaf53
zenious <7553066+Zenious@users.noreply.github.com>
2019-09-24 18:23:10 +0800

    parent* now also extract uses and modifies

--- Team00/Code00/src/spa/src/DesignExtractor.h
+++ Team00/Code00/src/spa/src/DesignExtractor.h
@@ -7,15 +7,17 @@
 #include "FollowsStarBank.h"
 #include "ParentBank.h"
 #include "ParentStarBank.h"
+#include "UsesBank.h"
+#include "ModifiesBank.h"
 
 class DesignExtractor
 {
 public:
     static void extract_follows_star(FollowsBank &bank_in, FollowsStarBank &bank_out);
-    static void extract_parent_star(ParentBank &bank_in, ParentStarBank &bank_out);
+    static void extract_parent_star(ParentBank &bank_in, ParentStarBank &bank_out, UsesBank &uses_bank, ModifiesBank &modifies_bank);
 
 private:
-    static void extract_further_parents_child(ParentBank &bank_in, ParentStarBank &bank_out, std::vector<int> parents, int child);
+    static void extract_further_parents_child(ParentBank &bank_in, ParentStarBank &bank_out, UsesBank &uses_bank, ModifiesBank &modifies_bank, std::vector<int> parents, int child);
 };
 
 #endif //AUTOTESTER_DESIGNEXTRACTOR_H
