c1d675ae941ee42368985dde3928574564b1a35a
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-24 18:01:49 +0800

    add tests and fix bugs

--- Team00/Code00/src/integration_testing/src/TestFrontEnd.cpp
+++ Team00/Code00/src/integration_testing/src/TestFrontEnd.cpp
@@ -36,5 +36,37 @@ TEST_CASE("Valid SIMPLE code")
 
         REQUIRE(assign_list == expected_result);
     }
+
+    SECTION("Check Constant Table")
+    {
+        std::vector<int> constant_list = pkb.get_all_constants();
+        std::vector<int> expected_result = { 1, 7, 0, 4 };
+
+        REQUIRE(constant_list == expected_result);
+    }
+
+    SECTION("Check Proc Table")
+    {
+        std::unordered_set<std::string> procedure_list = pkb.get_all_procedures();
+        std::unordered_set<std::string> expected_result = { "computeSeq" };
+
+        REQUIRE(procedure_list == expected_result);
+    }
+
+    /*SECTION("Check Modifies Table")
+    {
+        std::vector<int> modifies_list = pkb.get_all_modifies_statements();
+        std::vector<int> expected_result = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+        REQUIRE(modifies_list == expected_result);
+    }*/
+
+    SECTION("Check Uses Table")
+    {
+        std::vector<int> uses_list = pkb.get_all_uses_statements();
+        std::vector<int> expected_result = { 14, 6, 7, 8, 9, 10, 11, 12 };
+
+        REQUIRE(uses_list == expected_result);
+    }
 }
 
