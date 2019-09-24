245fe6c3f4d8f97c8a2c5bb8d1cc00a0d54a8d08
DESKTOP-56O063T\Benedict <benedictchua1996@gmail.com>
2019-09-24 16:06:23 +0800

    add front end pkb integration test

--- Team00/Code00/src/integration_testing/src/TestFrontEnd.cpp
+++ Team00/Code00/src/integration_testing/src/TestFrontEnd.cpp
@@ -1,15 +1,40 @@
 #include "TNode.h"
 #include "catch.hpp"
+#include <PKB.h>
+#include <Parser.h>
+
 using namespace std;
-void require(bool b)
-{
-    REQUIRE(b);
-}
 
-TEST_CASE("1st Test")
+TEST_CASE("Valid SIMPLE code")
 {
+    std::string simple_code = "procedure computeSeq {\nfirst = 1;\nd = 7;\nread b;\ncount = 0;\nread first;\nd = a;\nif (d > limit) then{\nwhile (count < limit) {\np = first; \nx = first; \nfirst = p; \ncount = count; \n} }\nelse { \nx = 4; \n }\nprint x;\n}";
 
-    TNode T;
-    require(1 == 1);
+    PKB pkb = PKB();
+    Parser parser = Parser(&pkb);
+
+    parser.parse(simple_code);
+
+    SECTION("Check Statement Table")
+    {
+        std::vector<int> stmt_list = pkb.get_all_statement_nums();
+        std::vector<int> expected_result = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+        REQUIRE(stmt_list == expected_result);
+    }
+
+    SECTION("Check Assign Table")
+    {
+        std::vector<int> assign_list = pkb.get_all_assigns();
+        std::vector<int> expected_result = { 1, 2, 4, 6, 9, 10, 11, 12, 13 };
+        REQUIRE(assign_list == expected_result);
+    }
+
+    SECTION("Check Variables Table")
+    {
+        std::unordered_set<std::string> assign_list = pkb.get_all_variables();
+        std::unordered_set<std::string> expected_result = { "first", "d", "b", 
+            "count", "first", "a", "limit", "p", "x" };
+
+        REQUIRE(assign_list == expected_result);
+    }
 }
 
