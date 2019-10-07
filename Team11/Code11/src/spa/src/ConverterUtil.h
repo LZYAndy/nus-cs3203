10dbbe79cde1d7c96f9eadc08f226c5695bc1be4
etjk <edricteojk@hotmail.com>
2019-10-08 00:44:03 +0800

    Implement converterutil and added test cases

--- Team11/Code11/src/spa/src/ConverterUtil.h
+++ Team11/Code11/src/spa/src/ConverterUtil.h
@@ -11,7 +11,27 @@
 class ConverterUtil
 {
 public:
+    /**
+     * Converts an infix expression into an prefix expression
+     * @param infix
+     * @return Returns the expression in prefix form.
+     */
     static std::string convert_infix_prefix(std::string infix);
+
+private:
+    /**
+     * Obtains the priority of an operator. The higher the value, the more priority it has.
+     * @param input
+     * @return Returns the priority of an operator.
+     */
+    static int get_opr_priority(std::string input);
+
+    /**
+     * Checks if an character is a valid operator.
+     * @param input
+     * @return Returns true if the input is valid and false otherwise.
+     */
+    static bool is_operator(char input);
 };
 
 #endif //AUTOTESTER_CONVERTERUTIL_H
