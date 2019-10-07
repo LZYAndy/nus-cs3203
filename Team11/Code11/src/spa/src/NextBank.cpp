915d52539b2e681998957618525de19dc74a9c03
Kelly9373 <42425964+Kelly9373@users.noreply.github.com>
2019-10-07 23:39:23 +0800

    Add NextBank

---
+++ Team11/Code11/src/spa/src/NextBank.cpp
@@ -0,0 +1,1 @@
+#include "NextBank.h"
---
+++ Team11/Code11/src/spa/src/NextBank.h
@@ -0,0 +1,16 @@
+#ifndef AUTOTESTER_NEXTBANK_H
+#define AUTOTESTER_NEXTBANK_H
+
+
+#include "Bank.h"
+
+class NextBank {
+public:
+
+
+private:
+    Bank<int, int> next_bank;
+};
+
+
+#endif //AUTOTESTER_NEXTBANK_H
