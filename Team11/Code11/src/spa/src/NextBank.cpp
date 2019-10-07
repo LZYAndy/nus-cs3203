e725f04f8447ce5e543de03a508399980ce5a45b
Kelly9373 <42425964+Kelly9373@users.noreply.github.com>
2019-10-08 00:12:41 +0800

    Add APIs in NextBank

--- Team11/Code11/src/spa/src/NextBank.cpp
+++ Team11/Code11/src/spa/src/NextBank.cpp
@@ -1,1 +1,54 @@
 #include "NextBank.h"
+
+bool NextBank::insert_next(int stmt1, int stmt2) {
+    if (stmt2 <= stmt1)
+    {
+        return false;
+    }
+    else
+    {
+        next_bank.put(stmt1, stmt2);
+        return true;
+    }
+}
+
+bool NextBank::is_next(int stmt1, int stmt2) {
+    std::vector<int> elements = next_bank.get(stmt1);
+    if(elements.size() == 0)
+    {
+        return false;
+    }
+    else
+    {
+        std::vector<int>::iterator ret;
+        ret = std::find(elements.begin(), elements.end(), stmt2);
+        if(ret == elements.end())
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+}
+
+bool NextBank::does_next_exists() {
+    return !next_bank.empty();
+}
+
+std::vector<int> NextBank::get_statements_previous(int statement) {
+    return next_bank.get_reverse(statement);
+}
+
+std::vector<int> NextBank::get_statements_next(int statement) {
+    return next_bank.get(statement);
+}
+
+std::vector<int> NextBank::get_all_previous() {
+    return next_bank.get_all_keys();
+}
+
+std::vector<int> NextBank::get_all_next() {
+    return next_bank.get_all_values();
+}
--- Team11/Code11/src/spa/src/NextBank.h
+++ Team11/Code11/src/spa/src/NextBank.h
@@ -6,7 +6,53 @@
 
 class NextBank {
 public:
+    /**
+     * Insert Next relationship between statement1 and statement2.
+     * @param stmt1
+     * @param stmt2
+     * @return Return true if the relationship is inserted successfully, otherwise false.
+     */
+    bool insert_next(int stmt1, int stmt2);
 
+    /**
+     * If there is a Next relationship between statement1 and statement2.
+     * @param stmt1
+     * @param stmt2
+     * @return Return true if there is a Next relationship between these two statements, otherwise false.
+     */
+    bool is_next(int stmt1, int stmt2);
+
+    /**
+     * If there exists at least one Next relationship in the program.
+     * @return Return true if there exists at least one Next relationship, otherwise false.
+     */
+    bool does_next_exists();
+
+    /**
+     * Get all statements that are the Previous statement of the input statement
+     * @param statement
+     * @return Return a vector of statements which are the previous statement of the input statement.
+     */
+    std::vector<int> get_statements_previous(int statement);
+
+    /**
+     * Get all statements that are the Next statement of the input statement
+     * @param statement
+     * @return Return a vector of statements which are the next statement of the input statement.
+     */
+    std::vector<int> get_statements_next(int statement);
+
+    /**
+     * Get all statements which are the in previous position in their Next relationships.
+     * @return Return all which are the in previous position in their Next relationships.
+     */
+    std::vector<int> get_all_previous();
+
+    /**
+     * Get all statements which are the in next position in their Next relationships.
+     * @return Return all which are the in next position in their Next relationships.
+     */
+    std::vector<int> get_all_next();
 
 private:
     Bank<int, int> next_bank;
