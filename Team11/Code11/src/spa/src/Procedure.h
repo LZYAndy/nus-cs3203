5c96accec7ab2c4e22ded8dc309ebba3c0da756b
liuxuanian <liuxuan2472@gmail.com>
2019-10-22 02:43:15 +0800

    comment on the code.

--- Team11/Code11/src/spa/src/Procedure.h
+++ Team11/Code11/src/spa/src/Procedure.h
@@ -12,10 +12,23 @@ private:
     std::string body_of_proc;
 
 public:
-    // Constructor
+    /**
+     * Constructor of a Procedure.
+     * @param name the name of the procedure.
+     * @param body the body of the procedure.
+     */
     Procedure(std::string name, std::string body);
 
+    /**
+     * The name getter.
+     * @return the name of this procedure.
+     */
     std::string get_name();
+
+    /**
+     * The body getter.
+     * @return the body of the procedure.
+     */
     std::string get_body();
 };
 
