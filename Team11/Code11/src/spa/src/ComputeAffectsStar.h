8356f5229cc8f5542353f92bfad30a556d198b85
zenious <7553066+Zenious@users.noreply.github.com>
2019-10-25 23:14:16 +0800

    add docs for affectsStar

--- Team11/Code11/src/spa/src/ComputeAffectsStar.h
+++ Team11/Code11/src/spa/src/ComputeAffectsStar.h
@@ -10,27 +10,42 @@ class ComputeAffectsStar
 {
 public:
     /**
-     * affects*(1,2)
+     * Check if one assignment statement affects the another assignment directly or indirectly.
+     * That is to say Affects(1, 2).
+     * @param assignment1 assignment statement to check if affects others
+     * @param assignment2 assignment statement that is affected
+     * @return true if Affects* relationship holds for the 2 assignment statement.
      */
     bool is_affects_star(PKB& pkb, int assignment1, int assignment2);
     /**
-     * affects*(1,a)
+     * Get all assignment statement that affects the quried assignment statement directly or indirectly.
+     * That is to say Affects*(1, a).
+     * @param assignment quried assignment statement
+     * @return vector containing all the statement numbers of assignment statement affected by quried assignment.
      */
     std::vector<int> get_affects_star(PKB& pkb, int assignment);
     /**
-     * affects*(a, 1)
+     * Get all assignment statement that affected the quried assignment statement directly or indirectly.
+     * That is to say Affects*(a, 1).
+     * @param assignment quried assignment statement
+     * @return vector containing all the statement numbers of assignment statement affects quried assignment.
      */
     std::vector<int> get_affected_star(PKB& pkb, int assignment);
-        /**
-     * affects*(1,a)
-     */
-    std::vector<int> get_all_affects_star(int assignment);
     /**
-     * affects*(a, 1)
+     * Get all assignemnt statement that can be affected by other assignment statements.
+     * This is to say Affcets*(_, a).
+     * @return vector containing all statement numbers of assignment statement that could be affected by other assignment statement.
      */
-    std::vector<int> get_all_affected_star(int assignment);
+    std::vector<int> get_all_affects_star();
     /**
-     * affected(a, a1)
+     * Get all assignemnt statement that can be affects other assignment statements.
+     * This is to say Affcets*(a, _).
+     * @return vector containing all statement numbers of assignment statement that could be affects other assignment statement.
+     */
+    std::vector<int> get_all_affected_star();
+    /**
+     * Get all Affects* relationship that exists.
+     * @return unodered_map that contains all the affects* relationship.
      */
     std::unordered_map<int, std::vector<int>> get_all_affects_star_relationship(PKB &pkb);
 
