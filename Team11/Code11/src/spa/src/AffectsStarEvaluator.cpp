255fa8afb95d546cc58e8eded087848b7dde7564
刘照宇 <e0253678@u.nus.edu>
2019-11-02 23:48:53 +0800

    uncommit affects, affects* and add integration test

--- Team11/Code11/src/spa/src/AffectsStarEvaluator.cpp
+++ Team11/Code11/src/spa/src/AffectsStarEvaluator.cpp
@@ -1,86 +1,86 @@
-//#include "AffectsStarEvaluator.h"
-//
-//unordered_map<string, vector<string>> AffectsStarEvaluator::evaluate_non_trivial(pql_dto::Entity &first_param,
-//        pql_dto::Entity &second_param, PKB &PKB)
-//{
-//    unordered_map<string, vector<string>> result;
-//    unordered_map<string, vector<string>> empty_map;
-//    string first_name = first_param.get_entity_name();
-//    string second_name = second_param.get_entity_name();
-//
-//    if (first_param.is_entity_declared())
-//    {
-//        if (second_param.get_entity_type() == EntityType::ANY)
-//        {
-//            // e.g. Affects*(a, _)
-//            vector<int> int_vec = PKB.get_all_assigns_affect();
-//            result = QueryUtility::mapping(first_param, int_vec, PKB);
-//        }
-//        else if (QueryUtility::is_statement_num(second_param))
-//        {
-//            // e.g. Affects*(a, 2)
-//            vector<int> int_vec = PKB.get_assigns_affect_star(stoi(second_name));
-//            result = QueryUtility::mapping(first_param, int_vec, PKB);
-//        }
-//        else
-//        {
-//            // e.g. Affects*(a1, a2), where a1 could equal to a2
-//            unordered_map<int, vector<int>> int_map = PKB.get_all_affects_star_relationship();
-//            result = QueryUtility::mapping(first_param, second_param, int_map, PKB);
-//        }
-//    }
-//
-//    if (second_param.is_entity_declared())
-//    {
-//        if (first_param.get_entity_type() == EntityType::ANY)
-//        {
-//            // e.g. Affects*(_, a)
-//            vector<int> int_vec = PKB.get_all_assigns_affected();
-//            result = QueryUtility::mapping(second_param, int_vec, PKB);
-//        }
-//        else if (QueryUtility::is_statement_num(first_param))
-//        {
-//            // e.g. Affects*(1, a)
-//            vector<int> int_vec = PKB.get_assigns_affected_star_by(stoi(first_name));
-//            result = QueryUtility::mapping(second_param, int_vec, PKB);
-//        }
-//    }
-//    return result;
-//}
-//
-//bool AffectsStarEvaluator::evaluate_trivial(pql_dto::Entity &first_param,
-//        pql_dto::Entity &second_param, PKB &PKB)
-//{
-//    bool result = false;
-//    string first_name = first_param.get_entity_name();
-//    string second_name = second_param.get_entity_name();
-//
-//    if (first_param.get_entity_type() == EntityType::ANY)
-//    {
-//        if (second_param.get_entity_type() == EntityType::ANY)
-//        {
-//            // e.g. Affects*(_, _)
-//            result = PKB.does_affects_exist();
-//        }
-//        else if (QueryUtility::is_statement_num(second_param))
-//        {
-//            // e.g. Affects*(_, 2)
-//            result = !PKB.get_assigns_affect_star(stoi(second_name)).empty();
-//        }
-//    }
-//
-//    if (QueryUtility::is_statement_num(first_param))
-//    {
-//        if (second_param.get_entity_type() == EntityType::ANY)
-//        {
-//            // e.g. Affects*(1, _)
-//            result = !PKB.get_assigns_affected_star_by(stoi(first_name)).empty();
-//        }
-//        else if (QueryUtility::is_statement_num(second_param))
-//        {
-//            // e.g. Affects*(1, 2)
-//            result = PKB.is_affects_star(stoi(first_name), stoi(second_name));
-//        }
-//    }
-//    return result;
-//}
+#include "AffectsStarEvaluator.h"
+
+unordered_map<string, vector<string>> AffectsStarEvaluator::evaluate_non_trivial(pql_dto::Entity &first_param,
+        pql_dto::Entity &second_param, PKB &PKB)
+{
+    unordered_map<string, vector<string>> result;
+    unordered_map<string, vector<string>> empty_map;
+    string first_name = first_param.get_entity_name();
+    string second_name = second_param.get_entity_name();
+
+    if (first_param.is_entity_declared())
+    {
+        if (second_param.get_entity_type() == EntityType::ANY)
+        {
+            // e.g. Affects*(a, _)
+            vector<int> int_vec = PKB.get_all_assigns_affect();
+            result = QueryUtility::mapping(first_param, int_vec, PKB);
+        }
+        else if (QueryUtility::is_statement_num(second_param))
+        {
+            // e.g. Affects*(a, 2)
+            vector<int> int_vec = PKB.get_affects_star(stoi(second_name));
+            result = QueryUtility::mapping(first_param, int_vec, PKB);
+        }
+        else
+        {
+            // e.g. Affects*(a1, a2), where a1 could equal to a2
+            unordered_map<int, vector<int>> int_map = PKB.get_all_affects_star_relationship();
+            result = QueryUtility::mapping(first_param, second_param, int_map, PKB);
+        }
+    }
+
+    if (second_param.is_entity_declared())
+    {
+        if (first_param.get_entity_type() == EntityType::ANY)
+        {
+            // e.g. Affects*(_, a)
+            vector<int> int_vec = PKB.get_all_assigns_affected();
+            result = QueryUtility::mapping(second_param, int_vec, PKB);
+        }
+        else if (QueryUtility::is_statement_num(first_param))
+        {
+            // e.g. Affects*(1, a)
+            vector<int> int_vec = PKB.get_affected_star(stoi(first_name));
+            result = QueryUtility::mapping(second_param, int_vec, PKB);
+        }
+    }
+    return result;
+}
+
+bool AffectsStarEvaluator::evaluate_trivial(pql_dto::Entity &first_param,
+        pql_dto::Entity &second_param, PKB &PKB)
+{
+    bool result = false;
+    string first_name = first_param.get_entity_name();
+    string second_name = second_param.get_entity_name();
+
+    if (first_param.get_entity_type() == EntityType::ANY)
+    {
+        if (second_param.get_entity_type() == EntityType::ANY)
+        {
+            // e.g. Affects*(_, _)
+            result = PKB.does_affects_exist();
+        }
+        else if (QueryUtility::is_statement_num(second_param))
+        {
+            // e.g. Affects*(_, 2)
+            result = !PKB.get_affects_star(stoi(second_name)).empty();
+        }
+    }
+
+    if (QueryUtility::is_statement_num(first_param))
+    {
+        if (second_param.get_entity_type() == EntityType::ANY)
+        {
+            // e.g. Affects*(1, _)
+            result = !PKB.get_affected_star(stoi(first_name)).empty();
+        }
+        else if (QueryUtility::is_statement_num(second_param))
+        {
+            // e.g. Affects*(1, 2)
+            result = PKB.is_affects_star(stoi(first_name), stoi(second_name));
+        }
+    }
+    return result;
+}
